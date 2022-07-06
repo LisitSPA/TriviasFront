c2bbf9ff0f0e66a143c151a048005050c7147297
Felipe Segovia Rodríguez <fsegovia@lisit.cl>
2022-07-06 10:11:55 -0400

    prueba

--- src/components/InputScore.tsx
+++ src/components/InputScore.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import {Box, Typography} from "@mui/material";
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
 const InputScore = ({ score }: any) => {
   return (
-    <Box sx={{
-      backgroundColor: "#8C8B8E",
-      borderRadius: "20px",
-      width: "20%",
-      height: "30px"
-    }}>
-      <Typography variant={"h6"} color={"white"}style={{fontWeight: "bold", textAlign: "center"}}>{score}</Typography>
+    <Box
+      sx={{
+        backgroundColor: "#8C8B8E",
+        borderRadius: "20px",
+        width: "20%",
+        height: "30px",
+      }}
+    >
+      <Typography
+        variant={"h6"}
+        color={"white"}
+        style={{ fontWeight: "bold", textAlign: "center" }}
+      >
+        {score}
+      </Typography>
     </Box>
   );
 };
